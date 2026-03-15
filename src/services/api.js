7a275dac64a6ccc5993eb595aaafe2c9f0d93605
RishithaJangiti <rishithajangiti23@gmail.com>
2026-03-15 18:19:07 +0530

    final commit

--- src/services/api.js
+++ src/services/api.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://randomideagenerator-backend.onrender.com/api/ideas"
+  baseURL: "https://randomideagenerator-backend.onrender.com/api/ideas",
 });
 
-export const getAllIdeas = () => API.get("");
+export const getAllIdeas = () => API.get("/");
 export const getIdeaById = (id) => API.get(`/${id}`);
 export const getRandomIdea = () => API.get("/random");
-export const createIdea = (idea) => API.post("", idea);
+export const createIdea = (idea) => API.post("/", idea);
 export const updateIdea = (id, idea) => API.put(`/${id}`, idea);
 export const deleteIdea = (id) => API.delete(`/${id}`);
